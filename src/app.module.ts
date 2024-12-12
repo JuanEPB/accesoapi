1f2dafb1dfb889484659b35fe2bef63a4c906cb1
JuanEPB <JuanEPB20@outlook.com>
2024-12-12 16:56:24 -0600

    api final

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/final'),
+    MongooseModule.forRoot('mongodb://localhost:27017/test'),
     SensorModule,
     UsersModule,
     AuthModule,
